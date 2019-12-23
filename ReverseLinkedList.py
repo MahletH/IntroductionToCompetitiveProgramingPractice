6b462a8e82859c63561b69ea4a80aad6d0de6807
MahletH <47258462+MahletH@users.noreply.github.com>
2019-12-23 22:45:49 +0300

    Add ReverseLinkedList.py

---
+++ ReverseLinkedList.py
@@ -0,0 +1,68 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def reverseList(self, head: ListNode) -> ListNode:
+        if(head==None):
+            return None
+        temp=head
+        node=ListNode(temp.val)
+        while(temp.next!=None):
+            temp=temp.next
+            headtwo=ListNode(temp.val)
+            headtwo.next=node
+            node=headtwo
+        return node
+
+def stringToIntegerList(input):
+    return json.loads(input)
+
+def stringToListNode(input):
+    # Generate list from the input
+    numbers = stringToIntegerList(input)
+
+    # Now convert that list into linked list
+    dummyRoot = ListNode(0)
+    ptr = dummyRoot
+    for number in numbers:
+        ptr.next = ListNode(number)
+        ptr = ptr.next
+
+    ptr = dummyRoot.next
+    return ptr
+
+def listNodeToString(node):
+    if not node:
+        return "[]"
+
+    result = ""
+    while node:
+        result += str(node.val) + ", "
+        node = node.next
+    return "[" + result[:-2] + "]"
+
+def main():
+    import sys
+    import io
+    def readlines():
+        for line in io.TextIOWrapper(sys.stdin.buffer, encoding='utf-8'):
+            yield line.strip('\n')
+
+    lines = readlines()
+    while True:
+        try:
+            line = next(lines)
+            head = stringToListNode(line);
+            
+            ret = Solution().reverseList(head)
+
+            out = listNodeToString(ret);
+            print(out)
+        except StopIteration:
+            break
+
+if __name__ == '__main__':
+    main()
