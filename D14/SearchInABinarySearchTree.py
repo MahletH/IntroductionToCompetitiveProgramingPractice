cf177238acd4b8df287fe78b63ed6435415b78d8
MahletH <47258462+MahletH@users.noreply.github.com>
2019-12-26 20:56:55 +0300

    Add SearchInABinarySearchTree.py

---
+++ D14/SearchInABinarySearchTree.py
@@ -0,0 +1,19 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def searchBST(self, root: TreeNode, val: int) -> TreeNode:
+        if(root==None):
+            return None
+        if(root.val==val):
+            return root
+        elif(val<root.val):            
+            root=self.searchBST(root.left,val)
+        
+        elif(val>root.val):
+            root=self.searchBST(root.right,val)
+        return root
